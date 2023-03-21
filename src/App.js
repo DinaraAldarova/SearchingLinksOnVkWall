6e98ac8370b0ce7c6176f0a02ba5b5e4ef8d17ba
Dinara Aldarova <dinara_aldarova@mail.ru>
2023-03-21 12:46:58 +0500

    .

--- src/App.js
+++ src/App.js
@@ -15,39 +15,67 @@ import Home from "./panels/Home";
 import Reposts from "./panels/Reposts";
 import Links from "./panels/Links";
 import ErrorAuth from "./panels/ErrorAuth";
+import { func } from "prop-types";
 
 const App = () => {
+	// application
 	const appId = 51537347;
 	const [scheme, setScheme] = useState("bright_light");
 	const [activePanel, setActivePanel] = useState("home");
-	const [fetchedUser, setUser] = useState(null);
-	const [popout, setPopout] = useState(<ScreenSpinner size="large" />);
+	const [popoutWait, setPopoutWait] = useState(<ScreenSpinner size="large" />);
+
+	// data
 	const [apiToken, setApiToken] = useState(null);
+	const [posts, setPosts] = useState([]);
 
 	useEffect(() => {
 		bridge.subscribe(({ detail: { type, data } }) => {
 			if (type === "VKWebAppUpdateConfig") {
 				setScheme(data.scheme);
 			}
-		});
+		})
 
-		async function fetchData() {
-			const user = await bridge.send("VKWebAppGetUserInfo");
-			setUser(user);
-			setPopout(null);
-		}
-		fetchData();
 
-		async function getApiToken() {
-			const getAuthTokenRusult = await bridge.send("VKWebAppGetAuthToken", {
-				app_id: appId,
-				scope: "wall",
-			});
-			console.log("access_token", getAuthTokenRusult);
-			setApiToken(getAuthTokenRusult.access_token);
-		}
-		getApiToken();
+		getApiToken()
+			.then(getPosts())
+			.then(setPopoutWait(null));
 	}, []);
+
+	async function getApiToken() {
+		const getAuthTokenResult = await bridge.send("VKWebAppGetAuthToken", {
+			app_id: appId,
+			scope: "wall",
+		});
+		setApiToken(getAuthTokenResult.access_token);
+		console.log(getAuthTokenResult.access_token);
+	};
+
+	async function getPosts() {
+		const callAPIMethodResult = await bridge.send("VKWebAppCallAPIMethod", {
+			method: "wall.get",
+			params: {
+				access_token: apiToken,
+				count: 100,
+				// extended: 1,
+				v: "5.131"
+			}
+		});
+		let countPosts = callAPIMethodResult.response.count;
+		setPosts(callAPIMethodResult.response.items);
+	};
+
+	async function getPosts(count, offset = 0) {
+		const callAPIMethodResult = await bridge.send("VKWebAppCallAPIMethod", {
+			method: "wall.get",
+			params: {
+				access_token: apiToken,
+				count: count,
+				offset: offset,
+				v: "5.131"
+			}
+		});
+		return callAPIMethodResult.response.items;
+	}
 
 	const go = (e) => {
 		setActivePanel(e.currentTarget.dataset.to);
@@ -57,7 +85,7 @@ const App = () => {
 		<ConfigProvider scheme={scheme}>
 			<AdaptivityProvider>
 				<AppRoot>
-					<SplitLayout popout={popout}>
+					<SplitLayout popout={popoutWait}>
 						<SplitCol>
 							<View activePanel={activePanel}>
 								<Home id="home" fetchedUser={fetchedUser} go={go} />
